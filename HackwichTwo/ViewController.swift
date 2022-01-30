6d480ac5c37c7824dcca648088b207abd80b7c29
Tiradoe1 <tiradoe@hawaii.edu>
2022-01-30 09:08:30 -1000

    Added object (pressMeButtonPressed)

--- HackwichTwo/ViewController.swift
+++ HackwichTwo/ViewController.swift
@@ -11,9 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //this is the code block for func viewDidload
         // Do any additional setup after loading the view.
     }
-
-
-}
+    
+    
+    
+    
+    
+    @IBAction func pressMeButtonPressed(_ sender: Any) {
+    
+        print("Hello World")
+        
+        
+    }
+    
+        
+      
+        
+        
+        
+        
+    }
 
